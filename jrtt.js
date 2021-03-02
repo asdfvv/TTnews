253040843d0a1fa710fea961d1207ee8c71e6635
asdfvv <13.y@live.com>
2021-03-02 18:06:30 +0800

    Update jrtt.js

--- jrtt.js
+++ jrtt.js
@@ -179,7 +179,7 @@ Object.keys(readurl).forEach((item) => {
           readkeyArr.push(readkey[item])
         }
     });
-    console.log(`============ 脚本执行-国际标准时间(UTC)：${new Date().toLocaleString() - 8 * 60 * 60 * 1000}  =============\n`)
+    console.log(`============ 脚本执行-国际标准时间(UTC)：${new Date().toLocaleString() }  =============\n`)
     console.log(`============ 脚本执行-北京时间(UTC+8)：${new Date(new Date().getTime() ).toLocaleString()}  =============\n`)
  } else {
     signurlArr.push($.getdata('signurl'))
