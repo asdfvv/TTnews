8022de3d30621ea64063626fa47d2432bf1ee090
asdfvv <13.y@live.com>
2021-03-17 20:03:30 +0800

    Add files via upload

--- kkd.js
+++ kkd.js
@@ -72,9 +72,9 @@ if (isGetCookie) {
    $.done()
 } 
 if ($.isNode()) {
-  if (process.env.KKDHEADER && process.env.KKDHEADER.indexOf('|') > -1) {
-   kkdheader = process.env.KKDHEADER.split('|');
-   console.log(`您选择的是用"|"隔开\n`)
+  if (process.env.KKDHEADER && process.env.KKDHEADER.indexOf('#') > -1) {
+   kkdheader = process.env.KKDHEADER.split('#');
+   console.log(`您选择的是用"#"隔开\n`)
   }
   else if (process.env.KKDHEADER && process.env.KKDHEADER.indexOf('\n') > -1) {
    kkdheader = process.env.KKDHEADER.split('\n');
@@ -82,16 +82,16 @@ if ($.isNode()) {
   } else {
    kkdheader = process.env.KKDHEADER.split()
   };
-  if (process.env. KKDCOOKIE&& process.env.KKDCOOKIE.indexOf('|') > -1) {
-   kkdcookie = process.env.KKDCOOKIE.split('|');
+  if (process.env. KKDCOOKIE&& process.env.KKDCOOKIE.indexOf('#') > -1) {
+   kkdcookie = process.env.KKDCOOKIE.split('#');
   }
   else if (process.env.KKDCOOKIE && process.env.KKDCOOKIE.split('\n').length > 0) {
    kkdcookie = process.env.KKDCOOKIE.split('\n');
   } else  {
    kkdcookie = process.env.KKDCOOKIE.split()
   };
-  if (process.env. KKDSIGN&& process.env.KKDSIGN.indexOf('|') > -1) {
-   kkdsign = process.env.KKDSIGN.split('|');
+  if (process.env. KKDSIGN&& process.env.KKDSIGN.indexOf('#') > -1) {
+   kkdsign = process.env.KKDSIGN.split('#');
   }
   else if (process.env.KKDSIGN && process.env.KKDSIGN.split('\n').length > 0) {
    kkdsign = process.env.KKDSIGN.split('\n');
